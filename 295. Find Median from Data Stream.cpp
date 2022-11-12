3fb4e9471c123d590b9304aeb1d08fc78224fbee
Vrishti Bansal <vrishti2012@gmail.com>
2022-11-12 15:51:10 +0530

    Add files via upload

---
+++ 295. Find Median from Data Stream.cpp
@@ -0,0 +1,25 @@
+class MedianFinder {
+public:
+    /** initialize your data structure here. */
+    MedianFinder() {}
+    
+    void addNum(int num) {
+        (left.empty() || num <= left.top()) ? left.push(num) : right.push(num);
+        if(left.size() > right.size() + 1){
+            right.push(left.top());
+            left.pop();
+        }
+        if(right.size() > left.size()){
+            left.push(right.top());
+            right.pop();
+        }
+    }
+    
+    double findMedian() {
+        return left.size() > right.size()? left.top() : (left.top() + right.top()) / 2.0;
+    }
+
+private:
+    priority_queue<int>left;
+    priority_queue<int, vector<int>, greater<int>>right;
+};
