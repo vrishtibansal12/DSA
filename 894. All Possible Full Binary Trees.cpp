cb9f50a3cd7fef54702783a3ea7175ffa6d372cb
Vrishti Bansal <vrishti2012@gmail.com>
2023-07-23 12:30:10 +0530

    Add files via upload

---
+++ 894. All Possible Full Binary Trees.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    unordered_map<int,vector<TreeNode*>>dp;
+    vector<TreeNode*> build(int n)
+    {
+        vector<TreeNode*>nodes;
+        if(n%2==0)
+            return nodes;
+        if(n==1)
+        {
+            TreeNode* node=new TreeNode(0);
+            nodes.push_back(node);
+            return nodes;
+        }
+        if(dp.find(n)!=dp.end())
+            return dp[n];
+        for(int i=1;i<n;i+=2)
+        {
+            vector<TreeNode*> left=build(i);
+            vector<TreeNode*> right=build(n-i-1);
+            for(auto l:left)
+            {
+                for(auto r:right)
+                {
+                    TreeNode* root=new TreeNode(0);
+                    root->left=l;
+                    root->right=r;
+                    nodes.push_back(root);
+                }
+            }  
+        }
+        return dp[n]= nodes;
+    }
+    vector<TreeNode*> allPossibleFBT(int n) 
+    {
+        return build(n);
+    }
+};
