d1d19a07bccddc511fbff2744943a2dc6be9b3fc
Vrishti Bansal <vrishti2012@gmail.com>
2023-09-12 11:51:50 +0530

    Add files via upload

---
+++ 1647. Minimum Deletions to Make Character Frequencies Unique.cpp
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minDeletions = function(s) {
+     const n = s.length;
+    const count = new Array(26).fill(0);
+    
+    for (const char of s) {
+        const idx = char.charCodeAt(0) - 97;
+        count[idx]++;
+    }
+    
+    
+    const set = new Set();
+    
+    let removed = 0;
+     
+    for (let i = 0; i < 26; i++) {
+        let freq = count[i];
+        
+        if (freq == 0) continue;
+        
+        let curr = freq;
+        
+        while (curr > 0 && set.has(curr)) {
+            curr--;
+        }
+        
+        set.add(curr);
+        removed += (freq - curr);
+    }
+    
+    return removed;
+};
