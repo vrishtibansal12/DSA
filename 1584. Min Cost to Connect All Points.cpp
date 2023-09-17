8ae7d9c79a43b935ec7c8eaf346754dcaf602f37
Vrishti Bansal <vrishti2012@gmail.com>
2023-09-17 12:57:55 +0530

    Update 1584. Min Cost to Connect All Points.cpp

--- 1584. Min Cost to Connect All Points.cpp
+++ 1584. Min Cost to Connect All Points.cpp
@@ -1,31 +1,56 @@
 class Solution {
 public:
-    int dist(int x1, int y1, int x2, int y2){
-        return abs (x1-x2) + abs(y1-y2);
-    }
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        if (points.size() < 1) return 0;
-        
-        unsigned sz = points.size();
-        vector<int> minDists(sz,0);
-		
-        int result = 0;
-        minDists[0] = INT_MAX;
-        for (auto i = 1 ; i < sz; i++) {
-            minDists[i] = dist(points[0][0],points[0][1],points[i][0],points[i][1]);
+    int findPar(int val , vector<int>& par){
+        if(par[val]==val){
+            return val;
         }
-		
-		
-        for (auto i = 1; i < sz; i++) {
-            auto it = min_element(minDists.begin(), minDists.end());
-            result += *it;
-            int index = it - minDists.begin();
-            *it = INT_MAX;
-            for (auto i = 0 ; i < sz; i++) {
-                if (minDists[i] == INT_MAX) continue;
-                minDists[i] = min(minDists[i], dist(points[i][0], points[i][1], points[index][0], points[index][1]));
+        return par[val] = findPar(par[val],par);
+    } 
+    bool Union(int val1 , int val2 , vector<int>& par , vector<int>& size){
+        int par1 = findPar(val1,par);
+        int par2 = findPar(val2,par);
+        if(par1==par2) return false ;
+        if( size[par1] > size[par2]){
+            par[par2] = par1;
+        }
+        else if(size[par2]>size[par1]){
+            par[par1]=par2;
+        }
+        else{
+            par[par2] = par1;
+            size[par1]++;
+        }
+        return true;
+    }  
+
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        vector<vector<int>> edges;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int dist = abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
+                vector<int> temp;
+                temp.push_back(dist);
+                temp.push_back(i);
+                temp.push_back(j);
+                edges.push_back(temp);
             }
         }
-        return result;        
+        sort(edges.begin() , edges.end());
+        vector<int> par(n,-1);
+        vector<int> size(n,1);
+        for(int i=0;i<n;i++) par[i]=i;
+        int ans=0;
+        for(auto &e: edges){
+            bool flag = Union(e[1],e[2],par,size);
+            // cout<<flag<<endl;
+            if(flag){
+                ans+=e[0];
+                // cout<<"**"<<endl;
+            }
+        }
+        return ans;
+
+        
     }
 };
